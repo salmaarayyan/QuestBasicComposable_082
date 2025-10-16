3ccd0d9791ba06eb1d0979ea71aec2d32ffe27a5
salmaarayyan <salmaarayyan@gmail.com>
2025-10-16 15:50:15 +0700

    Box kedua berisi gambar dan teks My Music

--- app/src/main/java/com/example/questbasiccomposable_082/TataLetak.kt
+++ app/src/main/java/com/example/questbasiccomposable_082/TataLetak.kt
@@ -134,3 +134,11 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
             }
         }
         Spacer(modifier = Modifier.height(10.dp))
+
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        )

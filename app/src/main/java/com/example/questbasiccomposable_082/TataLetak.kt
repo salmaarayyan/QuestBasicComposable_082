0e2e8366bf0eafe4d46a3261ecd5e1d8c08c753d
salmaarayyan <salmaarayyan@gmail.com>
2025-10-16 15:12:19 +0700

    Mendefinisikan composable column dasar dan mengatur tata letak column dengan padding di atas, kiri, kanan

--- app/src/main/java/com/example/questbasiccomposable_082/TataLetak.kt
+++ app/src/main/java/com/example/questbasiccomposable_082/TataLetak.kt
@@ -22,3 +22,13 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+@Composable
+fun TataletakColumn(modifier: Modifier) { // Mendefinisikan Composable Column dasar.
+    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) { // Mengatur tata letak vertikal (Column) dengan padding di atas, kiri, dan kanan.
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
